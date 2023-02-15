a5b9359a8e20c3743b37d57e2912b1f943e79433
Eirik <eirikalv@icloud.com>
2023-02-15 11:28:37 +0100

    Moved VoxelType to utils

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,14 @@ use bevy::prelude::Vec3;
 pub const CHUNK_SIZE: f32 = 50.0;
 pub const CHUNK_VOLUME: usize = (CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE) as usize;
 
+pub type ChunkData = [VoxelType; CHUNK_VOLUME];
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum VoxelType {
+    Air,
+    Block,
+}
+
 pub fn to_3d(pos: f32) -> Vec3 {
     Vec3::new(
         pos % CHUNK_SIZE,
