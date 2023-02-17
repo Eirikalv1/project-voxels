299fe2380b02732343f1c2fef12e45b83b99f6fb
Eirik <eirikalv@icloud.com>
2023-02-17 21:31:09 +0000

    broken

--- src/voxels/meshing/chunk_to_mesh.rs
+++ src/voxels/meshing/chunk_to_mesh.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use bevy::prelude::*;
 use bevy::render::mesh::Indices;
 use bevy::render::render_resource::PrimitiveTopology;
@@ -6,48 +8,59 @@ use super::helper_functions::*;
 use crate::utils::*;
 use crate::voxels::chunk::*;
 
-impl From<&Chunk> for Mesh {
-    fn from(chunk: &Chunk) -> Self {
-        let mut quad_poses: PositionData = vec![];
-        let mut quad_normals: NormalData = vec![];
-        let mut quad_uvs: UvData = vec![];
+pub fn to_mesh(chunk: &Chunk, chunks: HashMap<(usize, usize, usize), Chunk>) -> Mesh {
+    let mut quad_poses: PositionData = vec![];
+    let mut quad_normals: NormalData = vec![];
+    let mut quad_uvs: UvData = vec![];
 
-        for (pos1d, voxel_type) in chunk.voxels.iter().enumerate() {
-            if *voxel_type == VoxelType::Block {
-                for quad in 0..6 {
-                    let pos3d = to_3d(pos1d as f32);
-                    if should_create_quad(quad, &chunk.voxels, pos3d)
-                    //&& !quad_outside_chunk(quad, pos3d)
-                    {
-                        let (mut quad_pos, mut quad_normal, mut quad_uv) =
-                            get_quad_data(quad, pos3d, chunk.world_pos);
-                        quad_poses.append(&mut quad_pos);
-                        quad_normals.append(&mut quad_normal);
-                        quad_uvs.append(&mut quad_uv);
-                    }
+    for (pos1d, voxel_type) in chunk.voxels.iter().enumerate() {
+        if *voxel_type == VoxelType::Block {
+            for quad in 0..6 {
+                let pos3d = to_3d(pos1d as f32);
+                if should_create_quad(quad, &chunk.voxels, pos3d)
+                //&& !quad_outside_chunk(quad, pos3d)
+                {
+                    let (mut quad_pos, mut quad_normal, mut quad_uv) =
+                        get_quad_data(quad, pos3d, chunk.world_pos);
+                    quad_poses.append(&mut quad_pos);
+                    quad_normals.append(&mut quad_normal);
+                    quad_uvs.append(&mut quad_uv);
                 }
             }
         }
-
-        let mut indices: Vec<u32> = vec![];
-        for quad in 0..(quad_poses.len() / 4) {
-            indices.append(&mut vec![
-                4 * quad as u32,
-                1 + 4 * quad as u32,
-                2 + 4 * quad as u32,
-                2 + 4 * quad as u32,
-                3 + 4 * quad as u32,
-                4 * quad as u32,
-            ]);
-        }
-
-        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, quad_poses);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, quad_normals);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, quad_uvs);
-        mesh.set_indices(Some(Indices::U32(indices)));
-        mesh
     }
+
+    let mut indices: Vec<u32> = vec![];
+    for quad in 0..(quad_poses.len() / 4) {
+        indices.append(&mut vec![
+            4 * quad as u32,
+            1 + 4 * quad as u32,
+            2 + 4 * quad as u32,
+            2 + 4 * quad as u32,
+            3 + 4 * quad as u32,
+            4 * quad as u32,
+        ]);
+    }
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, quad_poses);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, quad_normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, quad_uvs);
+    mesh.set_indices(Some(Indices::U32(indices)));
+    mesh
+}
+
+fn get_adjacent_chunk(pos3d: Vec3, chunks: HashMap<(usize, usize, usize), &Chunk>) -> [Chunk; 6] {
+    let pos: (usize, usize, usize) = (pos3d.x as usize, pos3d.y as usize, pos3d.z as usize);
+
+    [
+        chunks[&(pos.0 + 1, pos.1, pos.2)].clone(),
+        chunks[&(pos.0 - 1, pos.1, pos.2)].clone(),
+        chunks[&(pos.0, pos.1 + 1, pos.2)].clone(),
+        chunks[&(pos.0, pos.1 - 1, pos.2)].clone(),
+        chunks[&(pos.0, pos.1, pos.2 + 1)].clone(),
+        chunks[&(pos.0, pos.1, pos.2 - 1)].clone(),
+    ]
 }
 
 /*
